c8b4afc2df4e586accf556adceb1b3b5297b647e
Robert McNeil (porty) <pppppppman@gmail.com>
2016-02-11 20:22:44 +1100

    Fix filemode, add mod time

--- client/client.go
+++ client/client.go
@@ -138,7 +138,8 @@ type File struct {
 
 func (f File) Attr(ctx context.Context, a *fuse.Attr) error {
 	a.Inode = 0
-	a.Mode = f.info.Mode & 0777
+	a.Mode = f.info.Mode
 	a.Size = uint64(f.info.Size)
+	a.Mtime = f.info.ModTime
 	return nil
 }
